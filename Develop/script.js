a8627e1d7f006212d8857963edca8e1616cdf6e0
David Coppock <106124456+dccoppock@users.noreply.github.com>
2022-10-11 03:08:06 -0400

    completed project

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,120 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //-----------Prompt for length
+  let pwLength = 8;
+  let confirmed = false;
+  while( !confirmed ){
+    pwLength = parseInt(prompt("How many characters?",8));
+    if(!Number.isNaN(pwLength) && pwLength>=8 && pwLength<=128){
+      confirmed=true;
+    } else if(Number.isNaN(pwLength)){
+      alert("Length must be a number.");
+    } else {
+      alert("Length must be at least 8 characters and no more than 128 characters.");
+    }
+  }
+  //-----------Prompt for character types
+  let includeLowercase = true;
+  let includeUppercase = true;
+  let includeNumbers = true;
+  let includeSpecial = true;
+
+  confirmed = false;
+  while( !confirmed ){
+    includeLowercase = confirm("Include lowercase characters?");
+    includeUppercase = confirm("Include uppercase characters?");
+    includeNumbers = confirm("Include numeric characters?");
+    includeSpecial = confirm("Include special characters?");
+
+    if( !(!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) ){
+      confirmed=true;
+    } else {
+      alert("At least one character type must be selected.");
+    }
+  }
+
+  let lowerCase = [];
+  for(let i = 97 ; i<=122 ; i++){
+    lowerCase.push(String.fromCharCode(i));
+  }
+
+  let upperCase = [];
+  for(let i = 65 ; i<=90 ; i++){
+    upperCase.push(String.fromCharCode(i));
+  }
+
+  let numChars = [];
+  for(let i = 48 ; i<=57 ; i++){
+    numChars.push(String.fromCharCode(i));
+  }
+
+  let specialChars = [];
+  for(let i = 32 ; i<=47 ; i++){
+    specialChars.push(String.fromCharCode(i));
+  }
+  for(let i = 58 ; i<=64 ; i++){
+    specialChars.push(String.fromCharCode(i));
+  }
+  for(let i = 91 ; i<=96 ; i++){
+    specialChars.push(String.fromCharCode(i));
+  }
+  for(let i = 123 ; i<=126 ; i++){
+    specialChars.push(String.fromCharCode(i));
+  }
+
+  let allChars = [];
+  if(includeLowercase){
+    allChars.push(...lowerCase);
+  }
+  if(includeUppercase){
+    allChars.push(...upperCase);
+  }
+  if(includeNumbers){
+    allChars.push(...numChars);
+  }
+  if(includeSpecial){
+    allChars.push(...specialChars);
+  }
+
+  //--------- Pick unique spots to insert required characters
+  const iLowercase = Math.floor(Math.random()*pwLength);
+  let iUppercase = Math.floor(Math.random()*pwLength);
+  while(iUppercase===iLowercase){
+    iUppercase = Math.floor(Math.random()*pwLength);
+  }
+  let iNumber = Math.floor(Math.random()*pwLength);
+  while(iNumber===iLowercase || iNumber===iUppercase){
+    iNumber = Math.floor(Math.random()*pwLength);
+  }
+  let iSpecial = Math.floor(Math.random()*pwLength);
+  while(iSpecial===iLowercase || iSpecial===iUppercase || iSpecial===iNumber){
+    iSpecial = Math.floor(Math.random()*pwLength);
+  }
+
+  console.log(allChars);
+
+  let pw = "";
+
+  for(let i = 0 ; i < pwLength ; i++){
+    if(includeLowercase && i===iLowercase){
+      pw+=pickRandom(lowerCase);
+    } else if(includeUppercase && i===iUppercase){
+      pw+=pickRandom(upperCase);
+    } else if(includeNumbers && i===iNumber){
+      pw+=pickRandom(numChars);
+    } else if(includeSpecial && i===iSpecial){
+      pw+=pickRandom(specialChars);
+    } else {
+      pw+=pickRandom(allChars);
+    }
+  }
+
+  return pw;
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +122,12 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function pickRandom(charList) {
+  const rand = Math.floor(Math.random() * charList.length);
+
+  return charList[rand];
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# PasswordGenerator
+# Password Generator
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+## Description
+
+The Password Generator prompts the user to provide criteria required for the generated password, then displays a randomly generated password that meets those criteria.
+
+## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## Installation
+
+Open index.html and click "Generate Password"
+
+## Usage
+
+Click "Generate Password" and follow the prompts.
+
+## License
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+## Contributing
+
+N/A
+
+## Tests
+
+Try it out!
+
+## Questions
+
+Please contact me via email with any questions.
+
+  - GitHub: [dccoppock](https://github.com/dccoppock)
+
+  - Email: [dccoppock@gmail.com](mailto:dccoppock@gmail.com)
